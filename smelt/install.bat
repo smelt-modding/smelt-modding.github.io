cac3a75f4002a24543a7b10b15306e56956a30aa
Ken <kenny2minecraft@gmail.com>
2017-07-14 17:33:18 +0800

    changed format, added help

--- smelt/install.bat
+++ smelt/install.bat
@@ -1,6 +1,14 @@
 @echo off
 if not exist logs (mkdir logs)
-if "%1" EQU "" (goto direct) else (goto param)
+if "%1" EQU "/?" (goto help) else (if "%1" EQU "" (goto direct) else (goto param))
+:help
+echo Installs a command.
+echo.
+echo SBPM INSTALL package [/U | /UPGRADE]
+echo.
+echo.  package         Specifies the name of the package to install.
+echo.  /U              Upgrades the specified package (used to avoid overwrite prompts)
+goto end
 :direct
 if not exist extension (mkdir extension && echo "extension" folder doesn't exist, made and terminated... && exit)
 echo Copy batch files...
@@ -8,7 +16,7 @@ echo Copy batch files... > logs\install.log
 copy extension\*.bat %USERPROFILE% >> logs\install.log
 goto init
 :param
-if "%1" EQU "install" (goto install) else (goto upgrade)
+if /I "%2" EQU "/upgrade" (goto upgrade) else (if /I "%2" EQU "/u" (goto upgrade))
 :install
 echo Downloading batch file...
 bitsadmin /transfer sbpm /download /priority normal https://smelt-batch.github.io/packages/%1.bat %USERPROFILE%\%1.bat > logs\install.log
@@ -23,10 +31,11 @@ echo Make init.cmd...
 echo Make init.cmd... >> logs\install.log
 echo @echo off > %USERPROFILE%\init.cmd
 for %%i in (extension\*.bat) do echo doskey %%~ni=%USERPROFILE%\%%~nxi $* >> %USERPROFILE%\init.cmd
-echo doskey install=%~dp0\install.bat $* >> %USERPROFILE%\init.cmd
+echo doskey sbpm=%~dp0\sbpm.bat $* >> %USERPROFILE%\init.cmd
 echo echo on >> %USERPROFILE%\init.cmd
 echo Edit registry...
 echo Edit registry... >> logs\install.log
 reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d "%"USERPROFILE"%init.cmd" /f >> logs\install.log
 echo Success!
 echo Success! >> logs\install.log
+:end
