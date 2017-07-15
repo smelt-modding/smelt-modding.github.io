8172d759ede596bcad4e9a506b999948445158c4
Ken <kenny2minecraft@gmail.com>
2017-07-15 18:54:02 +0800

    Update and rename smelt/setup.bat to smelt-batch/setup.bat

--- smelt-batch/setup.bat
+++ smelt-batch/setup.bat
@@ -3,7 +3,7 @@ mkdir extension
 mkdir logs
 echo. > logs\setup.log
 for %%i in (install.bat, update.cmd, uninstall.bat, reinstall.bat, sbpm.bat) do (
-	bitsadmin /transfer setup /download /priority normal https://smelt-modding.github.io/smelt/%%i %~dp0\%%i >> logs\setup.log
+	bitsadmin /transfer setup /download /priority normal https://smelt-modding.github.io/smelt-batch/%%i %~dp0\%%i >> logs\setup.log
 )
 call install.bat
 echo on
