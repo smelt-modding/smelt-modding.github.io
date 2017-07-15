6fc359f8a434f02a45bff7252459aa9954206aed
Ken <kenny2minecraft@gmail.com>
2017-07-15 13:33:14 +0800

    update update update.cmd (lol)

--- smelt/update.cmd
+++ smelt/update.cmd
@@ -1,14 +1,19 @@
 @echo off
-echo Remove old version...
-echo Remove old version... > %~dp0\logs\update.log
-call %~dp0\uninstall.bat
-echo Download new version...
-echo Download new version... >> %~dp0\logs\update.log
-for %%i in (%~dp0\*.bat) do (
-  del %%~nxi
-  bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
+if /I "%1" EQU "update" (
+	bitsadmin /transfer sbpm-update-update /download /priority normal https://smelt-modding.github.io/smelt/update.cmd %~dp0\update.bat
+	type %~dp0\update.bat > %~f0 && del %~dp0\update.bat
+) else (
+	echo Remove old version...
+	echo Remove old version... > %~dp0\logs\update.log
+	call %~dp0\uninstall.bat
+	echo Download new version...
+	echo Download new version... >> %~dp0\logs\update.log
+	for %%i in (%~dp0\*.bat) do (
+		del %%~nxi
+		bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
+	)
+	echo Install new version...
+	echo Install new version... >> %~dp0\logs\update.log
+	call %~dp0\install.bat
+	echo on
 )
-echo Install new version...
-echo Install new version... >> %~dp0\logs\update.log
-call %~dp0\install.bat
-echo on
