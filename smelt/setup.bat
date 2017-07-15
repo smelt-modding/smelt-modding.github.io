7692210a7c466fb21323ac145f21e69437c5806e
Ken <kenny2minecraft@gmail.com>
2017-07-15 14:42:59 +0800

    update after testing

--- smelt/setup.bat
+++ smelt/setup.bat
@@ -1,8 +1,9 @@
 @echo off
 mkdir extension
 mkdir logs
+echo. > logs\setup.log
 for %%i in (install.bat, update.cmd, uninstall.bat, reinstall.bat, sbpm.bat) do (
-	bitsadmin /transfer setup /download /priority normal https://smelt-modding.github.io/smelt/%%i %%i
+	bitsadmin /transfer setup /download /priority normal https://smelt-modding.github.io/smelt/%%i %~dp0\%%i >> logs\setup.log
 )
 call install.bat
 echo on
