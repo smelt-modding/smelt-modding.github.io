fe60556d0d8d1c7f6b44f5ec541533212df2acae
Ken <kenny2minecraft@gmail.com>
2017-07-15 13:14:21 +0800

    implementing update

--- smelt/sbpm.bat
+++ smelt/sbpm.bat
@@ -1,3 +1,15 @@
 @echo off
-if /I "%1" EQU "install" (call %~dp0\install.bat %2) else (call %~dp0\uninstall.bat %2)
+if /I "%1" EQU "install" (
+  call %~dp0\install.bat %2
+) else (
+  if /I "%1" EQU "uninstall" (
+    call %~dp0\uninstall.bat %2
+  ) else (
+    if /I "%1" EQU "reinstall" (
+      call %~dp0\reinstall.bat
+    ) else (
+      call %~dp0\update.bat
+    )
+  )
+)
 echo on
