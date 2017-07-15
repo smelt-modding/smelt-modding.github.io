0cadceb50a3720f0117c91c2b4cbb6fbde6345d5
Ken <kenny2minecraft@gmail.com>
2017-07-15 13:39:53 +0800

    fix path

--- smelt/update.cmd
+++ smelt/update.cmd
@@ -9,12 +9,11 @@ if /I "%1" EQU "update" (
 	echo Download new version...
 	echo Download new version... >> %~dp0\logs\update.log
 	for %%i in (%~dp0\*.bat) do (
-		del %%~nxi
+		del %%~dpnxi
 		bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
 	)
 	echo Install new version...
 	echo Install new version... >> %~dp0\logs\update.log
 	call %~dp0\install.bat
-	echo on
 )
 echo on
