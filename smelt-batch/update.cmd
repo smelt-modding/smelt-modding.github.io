6a0932e9227ac42da461db8899b750d290ae537d
Ken <kenny2minecraft@gmail.com>
2017-07-15 18:53:18 +0800

    Update and rename smelt/update.cmd to smelt-batch/update.cmd

--- smelt-batch/update.cmd
+++ smelt-batch/update.cmd
@@ -1,6 +1,6 @@
 @echo off
 if /I "%1" EQU "update" (
-	bitsadmin /transfer sbpm-update-update /download /priority normal https://smelt-modding.github.io/smelt/update.cmd %~dp0\update.bat
+	bitsadmin /transfer sbpm-update-update /download /priority normal https://smelt-modding.github.io/smelt-batch/update.cmd %~dp0\update.bat
 	type %~dp0\update.bat > %~f0 && del %~dp0\update.bat
 ) else (
 	echo Remove old version...
@@ -10,7 +10,7 @@ if /I "%1" EQU "update" (
 	echo Download new version... >> %~dp0\logs\update.log
 	for %%i in (%~dp0\*.bat) do (
 		del %%~dpnxi
-		bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
+		bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt-batch/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
 	)
 	echo Install new version...
 	echo Install new version... >> %~dp0\logs\update.log
