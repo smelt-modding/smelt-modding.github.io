82a2152746d9671d375e4db9d3e39dce9691dc55
Ken <kenny2minecraft@gmail.com>
2017-07-24 13:31:30 +0800

    See extended description
    Switching away from bitsadmin
    Also removed separate "update update" option - update.cmd now updates itself anyway

--- smelt-batch/update.cmd
+++ smelt-batch/update.cmd
@@ -1,18 +1,17 @@
 @echo off
-if /I "%1" EQU "update" (
-	bitsadmin /transfer sbpm-update-update /download /priority normal https://smelt-modding.github.io/smelt-batch/update.cmd %~dp0\update.bat
-	type %~dp0\update.bat > %~f0 && del %~dp0\update.bat
-) else (
-	echo Remove old version...
-	echo Remove old version... > %~dp0\logs\update.log
-	call %~dp0\uninstall.bat
-	echo Download new version...
-	echo Download new version... >> %~dp0\logs\update.log
-	for %%i in (%~dp0\*.bat) do (
-		del %%~dpnxi
-		bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt-batch/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
-	)
-	echo Install new version...
-	echo Install new version... >> %~dp0\logs\update.log
-	call %~dp0\install.bat
+echo Remove old version...
+echo Remove old version... > %~dp0\logs\update.log
+call %~dp0\uninstall.bat
+echo Download new version...
+echo Download new version... >> %~dp0\logs\update.log
+for %%i in (%~dp0\*.bat) do (
+	del %%~dpnxi
+	bitsadmin /transfer sbpm-update /download /priority normal https://smelt-modding.github.io/smelt-batch/%%~nxi %~dp0\%%~nxi >> %~dp0\logs\update.log
 )
+echo Install new version...
+echo Install new version... >> %~dp0\logs\update.log
+call %~dp0\install.bat
+echo Update update...
+echo Update update... >> %~dp0\logs\update.log
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt-batch/update.cmd', '%~dp0\update.bat')" >> %~dp0\logs\update.log
+type %~dp0\update.bat > %~f0 && del %~dp0\update.bat
