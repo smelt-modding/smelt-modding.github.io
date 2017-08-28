4dc88bcc49ea2e50ed8a0293a5b5e8a21e1f30f6
Kenny2github <kenny2minecraft@gmail.com>
2017-08-28 13:12:23 +0800

    No longer serving bash

--- smelt/setup.bat
+++ smelt/setup.bat
@@ -3,7 +3,7 @@ mkdir extension
 mkdir logs
 echo. > logs\setup.log
 for %%i in (install.bat, update.cmd, uninstall.bat, reinstall.bat, sbpm.bat) do (
-	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt-batch/%%i', '%~dp0\%%i')" >> logs\setup.log
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt/%%i', '%~dp0\%%i')" >> logs\setup.log
 )
 call install.bat
 echo on
--- smelt/update.cmd
+++ smelt/update.cmd
@@ -6,12 +6,12 @@ echo Download new version...
 echo Download new version... >> %~dp0\logs\update.log
 for %%i in (%~dp0\*.bat) do (
 	del %%~dpnxi
-	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt-batch/%%~nxi', '%~dp0\%%~nxi')" >> %~dp0\logs\update.log
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt/%%~nxi', '%~dp0\%%~nxi')" >> %~dp0\logs\update.log
 )
 echo Install new version...
 echo Install new version... >> %~dp0\logs\update.log
 call %~dp0\install.bat
 echo Update update...
 echo Update update... >> %~dp0\logs\update.log
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt-batch/update.cmd', '%~dp0\update.bat')" >> %~dp0\logs\update.log
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://smelt-modding.github.io/smelt/update.cmd', '%~dp0\update.bat')" >> %~dp0\logs\update.log
 type %~dp0\update.bat > %~f0 && del %~dp0\update.bat
